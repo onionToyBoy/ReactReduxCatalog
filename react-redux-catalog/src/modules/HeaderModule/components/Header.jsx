cafe7571f8cde381ed8de3352e242a28619c66a2
onionToyBoy <grazdaninzemlidobri@gmail.com>
2021-10-04 14:46:46 +0300

    React-router-dom has been installed

---
+++ react-redux-catalog/src/modules/HeaderModule/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styles from './Header.module.css'
+
+export const Header = () =>{
+
+return( 
+    <nav className={styles.container}>
+        
+    </nav>
+)
+
+}
